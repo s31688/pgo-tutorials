d7d27bce6de6517cf08e4446f8e5d6306fa16639
Agnieszka Sopotnicka <s31688@pjwstk.edu.pl>
2024-03-06 13:10:06 +0100

    Excercise 1

---
+++ Main/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+        if (input > 0) {
+            System.out.println("The number is positive");
+        } else if (input < 0) {
+            System.out.println("The number is negative");
+        } else {
+            System.out.println("The value equals 0");
+        }
+    }
+}
